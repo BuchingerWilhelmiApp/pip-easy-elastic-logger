fb4b775dfd98b6ccf60e6ae7054bd631dd46ef9a
Justin Guese <guese.justin@gmail.com>
2022-01-07 12:43:42 +0100

    default indexname functionality

--- src/easyelasticlogger/__init__.py
+++ src/easyelasticlogger/__init__.py
@@ -3,10 +3,13 @@ from requests import post
 class EasyLogger():
     HARDCODEDURL = "http://easy-elastic-logging-receiver/log"
     
-    def __init__(self, appname):
+    def __init__(self, appname, indexname = "error"):
         self.appname = appname
+        self.indexname = indexname
     
-    def log(self, message, indexname = "error", severity = "NORMAL"):
+    def log(self, message, indexname = None, severity = "NORMAL"):
+        if indexname is None:
+            indexname = self.indexname
         return post(self.HARDCODEDURL, json={"message": message, "indexname" : indexname, "app": self.appname, "severity" : severity})
 
 
